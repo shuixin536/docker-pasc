8eed8926a0a966e4baa490385967ae704a9d64cb
chenshuixin <shuxin536@gmail.com>
2018-03-15 19:37:16 +0800

    centos env sh

---
+++ centos_env.sh
@@ -0,0 +1,14 @@
+
+ANACONDA_VERSION=5.1.0 
+
+curl -L https://repo.continuum.io/archive/Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh -o Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh 
+
+/bin/bash Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh -b -p /opt/conda
+
+rm Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh
+
+export PATH="/opt/conda/bin:${PATH}"
+
+mv /usr/bin/python /usr/bin/python_old
+ln -s /opt/conda/bin/python /usr/bin/python
+
