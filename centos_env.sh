7d51c348f64f833867c49dd39cb75ab937d60379
chenshuixin <shuxin536@gmail.com>
2018-03-16 11:13:48 +0800

    update

--- centos_env.sh
+++ centos_env.sh
@@ -1,4 +1,7 @@
 
+yum install bzip2
+#yum groupinstall 'Development Tools' -y
+
 ANACONDA_VERSION=5.1.0 
 
 curl -L https://repo.continuum.io/archive/Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh -o Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh 
